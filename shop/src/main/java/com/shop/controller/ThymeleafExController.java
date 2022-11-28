085c66f382cac9e3933822aafb095997bf5ba998
zpka14 <zpka14@naver.com>
2022-11-28 17:37:34 +0900

    스프링 부트 버전 2.7.7 -> 2.7.6 다운그레이드 타임리프 예제

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class ThymeleafExController {
 
     @GetMapping(value = "/ex01")
-    public String thymeleafExample01(Model model) {
+    public String thymeleafExample01(Model model){
         model.addAttribute("data", "타임리프 예제 입니다.");
         return "thymeleafEx/thymeleafEx01";
     }
@@ -70,5 +70,22 @@ public class ThymeleafExController {
 
         model.addAttribute("itemDtoList", itemDtoList);
         return "thymeleafEx/thymeleafEx04";
+    }
+
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05(){
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model){
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07(){
+        return "thymeleafEx/thymeleafEx07";
     }
 }
