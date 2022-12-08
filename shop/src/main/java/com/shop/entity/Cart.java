48fb7f633520733b01b8f03af123e1238ffa2776
zpka14 <zpka14@naver.com>
2022-12-08 15:39:44 +0900

    Auditing 공통 속성 공통화 및 엔티티 지연로딩

--- shop/src/main/java/com/shop/entity/Cart.java
+++ shop/src/main/java/com/shop/entity/Cart.java
@@ -18,7 +18,7 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 }
--- shop/src/main/java/com/shop/entity/Item.java
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity{
 
     @Id
     @Column(name = "item_id")
@@ -35,7 +35,5 @@ public class Item {
 
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //상품 판매 상태
-    private LocalDateTime regTime; //등록 시간
-    private LocalDateTime updateTime; //수정 시간
 
 }
