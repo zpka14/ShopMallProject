ffb1690c637fa9eadfcc773784d0cc80328f4619
zpka14 <zpka14@naver.com>
2022-12-14 18:00:09 +0900

    상품등록 페이지 개발 중

--- shop/src/main/java/com/shop/controller/itemController.java
+++ shop/src/main/java/com/shop/controller/itemController.java
@@ -2,11 +2,23 @@ package com.shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import com.shop.dto.ItemFormDto;
 
+import com.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import javax.validation.Valid;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class itemController {
+
+    private final ItemService itemService;
 
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model){
@@ -14,4 +26,26 @@ public class itemController {
         return "/item/itemForm";
     }
 
+    @PostMapping(value = "/admin/item/new")
+    public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
+                          Model model, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList) {
+
+        if (bindingResult.hasErrors()) {
+            return "item/itemForm";
+        }
+
+        if (itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
+            return "item/itemForm";
+        }
+
+        try {
+            itemService.saveItem(itemFormDto, itemImgFileList);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
+            return "item/itemForm";
+        }
+
+        return "redirect:/";
+    }
 }
