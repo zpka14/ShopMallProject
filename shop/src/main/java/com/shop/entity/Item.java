07fb215c7be52035cd4b0ef27763d4600a17e665
zpka14 <zpka14@naver.com>
2023-01-10 18:02:17 +0900

    상품 주문하기 서비스 제작 - 1

--- shop/src/main/java/com/shop/entity/Item.java
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -2,6 +2,7 @@ package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
 import com.shop.dto.ItemFormDto;
+import com.shop.exception.OutOfStockException;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -46,4 +47,11 @@ public class Item extends BaseEntity{
         this.itemSellStatus = itemFormDto.getItemSellStatus();
     }
 
+    public void removeStock(int stockNumber){
+        int restStock = this.stockNumber - stockNumber;
+        if(restStock < 0){
+            throw new OutOfStockException("상품의 재고가 부족합니다. (현재 재고 수량 : " + this.stockNumber + ")");
+        }
+        this.stockNumber = restStock;
+    }
 }
