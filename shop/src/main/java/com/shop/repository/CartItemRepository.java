3169124e140a79d7c2f1c9160921e26b823b3b20
zpka14 <zpka14@naver.com>
2023-02-09 14:22:33 +0900

    상품 장바구니 담기 기능 추가

---
+++ shop/src/main/java/com/shop/repository/CartItemRepository.java
@@ -0,0 +1,11 @@
+/* 장바구니 Repo */
+package com.shop.repository;
+
+import com.shop.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{
+
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+}
--- shop/src/main/java/com/shop/repository/CartRepository.java
+++ shop/src/main/java/com/shop/repository/CartRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+    Cart findByMemberId(Long memberId);
 }
